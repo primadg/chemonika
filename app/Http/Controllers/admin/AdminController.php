69c917305bda5a9169a28dbeacd2111d120c7a90
krdq <krdq@protonmail.com>
2021-09-27 08:54:14 +0300

    edit_img function

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -45,15 +45,16 @@ class AdminController extends Controller
 
     public function editRu($id)
     {
-        $product =  DB::table('product_rus')->where('pos_id',$id)->first();
+        $product = DB::table('product_rus')->where('pos_id', $id)->first();
         $entries = DB::table('table_ps')->where('post_id', $id)->get();
-        return view("ru.card_edit",['product'=>$product, 'main_id'=>$id, 'entries'=>$entries]);
+        return view("ru.card_edit", ['product' => $product, 'main_id' => $id, 'entries' => $entries]);
     }
+
     public function editEn($id)
     {
-        $product =  DB::table('product_ens')->where('pos_id',$id)->first();
+        $product = DB::table('product_ens')->where('pos_id', $id)->first();
         $entries = DB::table('table_ps')->where('post_id', $id)->get();
-        return view("en.card_edit",['product'=>$product, 'main_id'=>$id, 'entries'=>$entries]);
+        return view("en.card_edit", ['product' => $product, 'main_id' => $id, 'entries' => $entries]);
     }
 
     public function updatePosts(Request $request)
@@ -76,49 +77,60 @@ class AdminController extends Controller
         }
 
 
-
-
-        if($request->file('img')){
+        if ($request->file('img')) {
             $path = $request->file('img')->store('public/img');
 
             $ex = $request->file('img')->extension();
-            if(!($ex=='jpg'||$ex=='png')){
-                return redirect()->back()->with('error','Неверный формат загруженного файла');
+            if (!($ex == 'jpg' || $ex == 'png')) {
+                return redirect()->back()->with('error', 'Неверный формат загруженного файла');
             }
             $product->img = Storage::url($path);
             // todo дописать
-            if($is_ukr){
-                $product_en = Product_en::find($this->getSecondaryTableId('product_ens',$id));
-                $product_ru = Product_ru::find($this->getSecondaryTableId('product_rus',$id));
-                $product_ru->img= $product_en->img = Storage::url($path);
+            if ($is_ukr) {
+                $product_en = Product_en::find($this->getSecondaryTableId('product_ens', $id));
+                $product_ru = Product_ru::find($this->getSecondaryTableId('product_rus', $id));
+                $product->img = $product_ru->img = $product_en->img = Storage::url($path);
                 $product_ru->save();
                 $product_en->save();
                 unset($product_ru);
                 unset($product_en);
+            } else {
+                $product_en = Product_en::find(DB::table("product_ens")->where('pos_id', $product->pos_id)->first()->id);
+                $product_ru = Product_ru::find(DB::table("product_rus")->where('pos_id', $product->pos_id)->first()->id);
+                $product_ukr = Product_ukr::find(DB::table("product_rus")->where('id', $product->pos_id)->first()->id);
+                $product_ukr->img = $product_ru->img = $product_en->img = Storage::url($path);
+                $product_ru->save();
+                $product_en->save();
+                $product_ukr->save();
+                unset($product_ru);
+                unset($product_en);
+                unset($product_ukr);
             }
         }
 
         try {
 
 
-        $product->name = $request->name;
-        $product->group = $request->group;
-        $product->description = $request->description;
-        $product->field_of_usage = $request->field_of_usage;
-        $product->Product_usage = $request->product_usage;
-        $product->Standart = $request->standart;
-        $product->Package = $request->package;
-        $product->Storage = $request->stogare;
+            $product->name = $request->name;
+            $product->group = $request->group;
+            $product->description = $request->description;
+            $product->field_of_usage = $request->field_of_usage;
+            $product->Product_usage = $request->product_usage;
+            $product->Standart = $request->standart;
+            $product->Package = $request->package;
+            $product->Storage = $request->stogare;
 
-        $product->save();
+            $product->save();
 
-        return redirect()->back();
-        } catch (\Exception $ex){
-            return redirect()->back()->with('error','Ошибка');
+            return redirect()->back();
+        } catch (\Exception $ex) {
+            return redirect()->back()->with('error', 'Ошибка');
         }
     }
-    private function getSecondaryTableId($table, $main_id){
-        return DB::table($table)->where('pos_id',$main_id)->first()->id;
+
+    private function getSecondaryTableId($table, $main_id)
+    {
+        return DB::table($table)->where('pos_id', $main_id)->first()->id;
     }
 
 
